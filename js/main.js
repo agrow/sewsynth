83fac5a027b30d2c8741ee60b030222bd7a9a711
April <grow.april@gmail.com>
2017-04-14 16:01:58 -0700

    fabricjs is working, but I don't think I like it...

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+var global = {
+	mainCanvasHandler: null,
+	calcHeight: 0,
+	calcWidth: 0
+};
+
+var saveCalculatedDimensions = function(){
+	global.calcHeight = $("#mainDiv").height();
+	global.calcWidth = $("#mainDiv").width();
+	console.log("calculating height & width... " + global.calcHeight + ", " + global.calcWidth);
+};
 
 // Handled by flex, but we may need to override that if it doesn't work with our drawing functions...
-var resizeSVG = function(){
+var resizeCanvasEvent = function(){
 	//console.log($(document).width());
 	//$("#svg_canvas").width($(document).width() - 160);
 	//$("#svg_canvas").height($(document).height() - 140);
@@ -10,26 +21,22 @@ var setGUIFunctions = function(){
 	// Put button/UI functions here, to be loaded when document is ready!
 };
 
-var drawBackground = function(start){
-	
+var initCanvas = function(){
+	saveCalculatedDimensions(); // Needed to initialize new canvas at an actual decent size...
+	global.mainCanvasHandler = new canvasHandler("canvas");
 };
-
-var drawDesign = function(start){
-	
-};
-
-var randomRestart = function(design){
-	console.log("Random restart...");
-	sizeGrid();
-	var x = Math.floor(numAcross/2);
-	var y = Math.floor(numDown/2);
-	
-	randomGrammarStart(design, x, y);
-	console.log("Random restart complete.");
-};
+///////////////////////////////////////////////////////
+/////////////// EVENT-BASED FUNCTIONS ////////////////
+///////////////////////////////////////////////////////
+window.addEventListener("resize", function(){
+	saveCalculatedDimensions();
+	if(global.mainCanvasHandler !== null){
+		global.mainCanvasHandler.setCanvasDimensions(global.calcWidth, global.calcHeight);
+	}
+});
 
 $( document ).ready(function() {
 	// Import the rest of the functions
-		
+	initCanvas();
 	console.log( "ready!" );
 });
